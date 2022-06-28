62c724cdc78c18d9aefdbe2c0d026b0acbe203a7
ArbenitBytyqi <arbenitbytyqi7@gmail.com>
2022-06-29 02:18:29 +0300

    Cart Updated

--- index.js
+++ index.js
@@ -53,21 +53,21 @@ let state = {
       id: 8,
       name: 'berry',
       price: 0.30,
-      inCart: 3,
+      inCart: 0,
       image: 'assets/icons/008-berry.svg',
     },
     {
       id: 9,
       name: 'blueberry',
       price: 0.30,
-      inCart: 3,
+      inCart: 0,
       image: 'assets/icons/009-blueberry.svg',
     },
     {
       id: 10,
       name: 'eggplant',
       price: 0.30,
-      inCart: 3,
+      inCart: 0,
       image: 'assets/icons/010-eggplant.svg',
     },
   ]
@@ -76,6 +76,10 @@ let state = {
 function getItemImagePath(item) {
   let id = String(item.id).padStart(3, '0')
   return `assets/icons/${id}-${item.name}.svg`
+}
+
+function getCartItems() {
+  return state.storeItems.filter(item => item.inCart > 0)
 }
 
 function getTotal() { }
@@ -111,30 +115,33 @@ function renderCartItems() {
   let cartUl = document.querySelector('.cart--item-list')
   cartUl.textContent = ''
 
-  let cartLi = document.createElement('li')
 
-  let itemImg = document.createElement('img')
-  itemImg.className = 'cart--item-list'
-  itemImg.src = 'assets/icons/001-beetroot.svg'
-  itemImg.alt = 'beetroot'
+  for (let item of state.storeItems) {
+    let cartLi = document.createElement('li')
 
-  let itemName = document.createElement('p')
-  itemName.textContent = 'beetroot'
+    let itemImg = document.createElement('img')
+    itemImg.className = 'cart--item-list'
+    itemImg.src = getItemImagePath(item)
+    itemImg.alt = item.name
 
-  let removeButton = document.createElement('button')
-  removeButton.className = 'quantity-btn remove-btn center'
-  removeButton.textContent = '-'
+    let itemName = document.createElement('p')
+    itemName.textContent = item.name
 
-  let quantity = document.createElement('span')
-  quantity.className = 'quantity-text center'
-  quantity.textContent = '1'
+    let removeButton = document.createElement('button')
+    removeButton.className = 'quantity-btn remove-btn center'
+    removeButton.textContent = '-'
 
-  let addButton = document.createElement('button')
-  addButton.className = 'quantity-btn add-btn center'
-  addButton.textContent = '+'
+    let quantity = document.createElement('span')
+    quantity.className = 'quantity-text center'
+    quantity.textContent = String(item.inCart)
 
-  cartLi.append(itemImg, itemName, removeButton, quantity, addButton)
-  cartUl.append(cartLi)
+    let addButton = document.createElement('button')
+    addButton.className = 'quantity-btn add-btn center'
+    addButton.textContent = '+'
+
+    cartLi.append(itemImg, itemName, removeButton, quantity, addButton)
+    cartUl.append(cartLi)
+  }
 }
 
 function renderTotal() {
@@ -145,7 +152,7 @@ function render() {
   renderStoreItems()
   renderCartItems()
   renderTotal()
+  getCartItems()
 }
 
 render()
-renderCartItems()
